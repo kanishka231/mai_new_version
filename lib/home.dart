d59306b2face4e1db51e099c29df1912c686a4cc
Kalyani-17spec <sawai.1@iitj.ac.in>
2022-09-26 15:08:29 +0530

    message

--- lib/home.dart
+++ lib/home.dart
@@ -14,7 +14,7 @@ class HomeScreen_1 extends StatelessWidget {
             color: Color(0xFF5E7A86),
             child: VideoItems(
             videoPlayerController: VideoPlayerController.asset(
-              'assets/videos/exa.mp4',
+              'assets/dogs.mp4',
             ),
             looping: true,
             autoplay: false,
